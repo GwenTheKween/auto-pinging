7a55a93f30957c7a705160602843c09a1d2b4c55
Bruno Larsen <blarsen@redhat.com>
2023-06-02 14:34:40 +0200

    factored out code figuring out the index of a branch to simplify my life

--- main.py
+++ main.py
@@ -28,13 +28,39 @@ def check_for_dependencies(args):
     return not_found
 
 def first_setup(path):
+    all_lines = []
     try:
         f = open(path, 'r')
+        all_lines = f.readlines()
         f.close()
     except:
         print(f"no file {path}. Creating an empty one")
         f = open(path, "w")
         f.close()
+
+    # Check if there is corruption in the file
+    # (multiple lines with the same index)
+    dic = {}
+    for line in all_lines:
+        b = line.split(',')[0]
+        if b in dic:
+            dic[b] += 1
+        else:
+            dic[b] = 1
+    for b, count in dic.items():
+        if count > 1:
+            print(f"branch {b} is being tracked {count} times! Fix this by manually editing {path}")
+
+# given a certain id BRANCH, find the index containing the related line
+def find_line_index(branch, all_lines):
+    ind = 0
+    for line in all_lines:
+        l_branch = line.split(',')[0]
+        if branch == l_branch:
+            return ind
+        ind += 1
+    # if we did not return in the loop, it isn't in the list of lines
+    return -1
 
 def start(args):
     # First check if the branch is already being monitored.
@@ -43,11 +69,7 @@ def start(args):
     saved_lines = []
     with open(database, "r") as f:
         saved_lines = f.readlines()
-    for line in saved_lines:
-        branch = line.split(',')[0]
-        if branch == args.branch:
-            repeat += 1
-            existing = line
+    index = find_line_index(args.branch, saved_lines)
 
     # Now calculate the line that will be stored
     ping_start = datetime.date.today() + datetime.timedelta(weeks = args.time)
@@ -55,23 +77,15 @@ def start(args):
     new_line = ','.join([args.branch, args.email, ping_string]) + '\n'
 
     #Check for errors and if user wants overwrite (if needed)
-    if repeat > 1:
-        print(f"{args.branch} repeats {repeat} times. Please fix this manually by changing {database}")
-        return
-    elif repeat == 1:
-        ping=existing.split(',')[2]
+    if index >= 0:
+        ping = saved_lines[index].split(',')[2]
         opt = input(f"{args.branch} is already in being monitored, PING_START is {ping}overwrite? (y/n)")
         if opt != 'y':
             print("exiting.")
             return
         else:
             print("overwritting")
-            new_lines = []
-            for line in saved_lines:
-                branch = line.split(',')[0]
-                if branch != args.branch:
-                    new_lines.append(line)
-            saved_lines = new_lines
+            del saved_lines[index]
 
     # Add the line to the saved_lines, then write them all to the file
     saved_lines.append(new_line)
