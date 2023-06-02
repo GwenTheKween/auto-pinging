5647e92d48483753f60d64b834ab1c0a387e562b
Bruno Larsen <blarsen@redhat.com>
2023-06-02 11:24:39 +0200

    added dependency checking

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python3
 import argparse
+import subprocess
+
+def check_for_dependencies(args):
+    deps = ['at']
+    if args.remind:
+        deps += ['notify-send']
+    else:
+        deps += ['git']
+
+    not_found = []
+    for d in deps:
+        proc = subprocess.run(['which', d], stdout=subprocess.DEVNULL)
+        if proc.returncode != 0:
+            not_found.append(d)
+    #specifically checking for git send-email
+    if args.remind == False:
+        proc = subprocess.run(['git', 'send-email'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = proc.stdout.decode('utf-8')
+        if not output.find("No patch files specified!"):
+            print(output)
+            not_found.append('git-send-email')
+    return not_found
 
 def start(args):
     print(f"starting a timer for the branch {args.id}, which will go off in {args.time}")
@@ -18,6 +40,7 @@ def main():
     parser.add_argument ("--dry-run", action="store_true", help="Do not run commands that change the state of the system, only print the commands that would be run")
     parser.add_argument("--id", required=True, nargs=1)
     parser.add_argument("--time", "-t", required=True, nargs=1)
+    parser.add_argument("--remind", "-r", required=False, action="store_true")
 
     # testing if I can use this for subcommands
     parser.add_argument("cmd", choices=["start", "reset", "remove"], help="Which command will be used.")
@@ -28,6 +51,12 @@ def main():
     #reset_parser = subparsers.add_parser("reset", help="reset the timer for pinging the given patch")
     #delete_parser = subparsers.add_parser("remove", help="remove the timer for pinging a patch")
     args = parser.parse_args()
+
+    missing = check_for_dependencies(args)
+    if missing != []:
+        print(f"Missing the following dependencies to run the script as requested: {missing}")
+        return
+
     if args.cmd == "start":
         start(args)
     elif args.cmd == "reset":
